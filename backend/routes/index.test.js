64c870da56cf4ade0d333d768e6385dbafc8d6b7
SynthCyraX <SynthCyraX@gmail.com>
2024-01-06 10:18:47 +0100

    Minor changes test suite

--- backend/routes/index.test.js
+++ backend/routes/index.test.js
@@ -1,17 +1,16 @@
-// Import app and request from testUtils.js
 const { app, request } = require('./testUtils');
-
-// Describe block for testing the GET route '/'
 describe('GET /', () => {
-  // Test case: Index route should return the title
   it('Index route returns title', (done) => {
-    // Making a GET request to the '/' endpoint using the 'request' function
     request(app)
-      // Expecting the response to have 'Content-Type' header of type JSON
+      .get('/')
       .expect('Content-Type', /json/)
-      // Expecting the response body to be { title: 'Express' }
-      .expect({ title: 'Express' })
-      // Expecting the HTTP status code to be 200
+      .expect({ index: 'get' })
       .expect(200, done);
+  });
+});
+
+describe('POST /api/signup', () => {
+  describe('User sign up process', () => {
+    // TODO: test the signup process
   });
 });
