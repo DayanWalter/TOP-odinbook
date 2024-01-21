3fe04dda311e23b5983fc14e96c1d8afb65ebfb6
SynthCyraX <SynthCyraX@gmail.com>
2024-01-21 09:06:57 +0100

    Messages pushed and pulled to/from posts_id

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -1,29 +1,32 @@
 const asyncHandler = require('express-async-handler');
 const Post = require('../models/post');
+const User = require('../models/user');
 // Validate and sanitize input at createUser
 const { body, validationResult } = require('express-validator');
-
+// TODO: Validate and sanitize
 const createPost = asyncHandler(async (req, res, next) => {
-  const userid = req.user._id;
   // Validate and sanitize input(body)
   const post = new Post({
-    author_id: userid,
+    author_id: req.user._id,
     content: req.body.content,
   });
-  // Add post._id to user._id
   // Save to database
-  post.save();
+  await post.save();
+
+  // Add post._id to posts_id
+  await User.findByIdAndUpdate(req.user._id, {
+    $push: { posts_id: post._id },
+  });
   res.json({ createPost: 'Route works', post });
 });
-// TODO:First add post_id to user
+// TODO:First add post_id to user at createPost
 const readAllFeedPosts = asyncHandler(async (req, res, next) => {
   res.json({ readAllFeedPosts: 'Route works' });
 });
-// TODO:First add post_id to user
+// TODO:First add post_id to user at createPost
 const readAllUserPosts = asyncHandler(async (req, res, next) => {
   res.json({ readAllUserPosts: 'Route works' });
 });
-
 const readPostById = asyncHandler(async (req, res, next) => {
   const searchedPost = await Post.findById(req.params.postid).exec();
   res.json({ readPostById: 'Route works', searchedPost });
@@ -44,6 +47,8 @@ const updatePost = asyncHandler(async (req, res, next) => {
   res.json({ updatePost: 'Route works', updatedPost });
 });
 const deletePost = asyncHandler(async (req, res, next) => {
+  // Check if the logged in user wrote the post
+
   // take the id from jwt
   const postIdToDelete = req.params.postid;
   const deletedPost = await Post.findByIdAndDelete(postIdToDelete).exec();
@@ -51,6 +56,13 @@ const deletePost = asyncHandler(async (req, res, next) => {
     res.status(404).json({ error: 'Post not found' });
     return;
   }
+
+  // Remove post._id from user.posts_id
+  await User.findByIdAndUpdate(
+    req.user._id,
+    { $pull: { posts_id: postIdToDelete } },
+    { new: true } // Return the updated user document
+  );
 
   res.json({ deletePost: 'Route works', deletedPost });
 });
