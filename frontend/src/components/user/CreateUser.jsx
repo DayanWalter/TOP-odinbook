4635e2f95982095902e62c32c22fd1f4fa041729
SynthCyraX <SynthCyraX@gmail.com>
2024-01-24 09:34:58 +0100

    Changed UserCreate.jsx

--- frontend/src/components/user/CreateUser.jsx
+++ frontend/src/components/user/CreateUser.jsx
@@ -1,86 +1,89 @@
-// CreateUser.jsx
 import { useState } from 'react';
 
 const CreateUser = () => {
-  const [user_name, setUsername] = useState('');
-  const [email, setEmail] = useState('');
+  const [userData, setUserData] = useState({
+    user_name: '',
+    email: '',
+    password: '',
+    repeatPassword: '',
+  });
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
 
-  const isValidUsername = (username) => username.length >= 6;
-  const isValidEmail = (email) => {
+  const validateForm = () => {
+    if (userData.user_name.length < 6) {
+      setError('Username must be at least 6 characters long.');
+      return false;
+    }
+
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  };
-  const isValidPassword = (password, repeatPassword) => {
-    return password === repeatPassword;
-  };
-  const isValidPasswordLength = (password, repeatPassword) => {
-    return password.length >= 6 && repeatPassword.length >= 6;
+    if (!emailRegex.test(userData.email)) {
+      setError('Invalid email format.');
+      return false;
+    }
+
+    if (userData.password !== userData.repeatPassword) {
+      setError('Passwords are not the same.');
+      return false;
+    }
+
+    if (userData.password.length < 6 || userData.repeatPassword.length < 6) {
+      setError('Passwords must be at least 6 characters long.');
+      return false;
+    }
+
+    setError('');
+    return true;
   };
 
   const handleCreateUser = async () => {
-    // Validate username and email
-    if (!isValidUsername(user_name)) {
-      setError([{ msg: 'Username must be at least 6 characters long.' }]);
+    if (!validateForm()) {
       return;
     }
 
-    if (!isValidEmail(email)) {
-      setError([{ msg: 'Invalid email format.' }]);
-      return;
-    }
-    if (!isValidPassword(password, repeatPassword)) {
-      setError([{ msg: 'Passwords arent the same' }]);
-      return;
-    }
-    if (!isValidPasswordLength(password, repeatPassword)) {
-      setError([{ msg: 'Passwords must be at least 6 characters long' }]);
-      return;
-    }
-    setError('');
-    // Parameters for the backend request
     const requestOptions = {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ user_name, email, password }),
+      body: JSON.stringify(userData),
     };
 
     try {
-      // Set loading state to true, indicating the start of the API request
       setLoading(true);
 
-      // Make the POST request to create a new user
       const response = await fetch(
         'http://localhost:3000/api/user/create',
         requestOptions
       );
-      // Parse the JSON data from the successful response
       const data = await response.json();
 
-      // Throw an error if the response indicates a failure
       if (!response.ok) {
-        setError(data.error.errors);
+        setError(data.error.errors[0].msg);
         return;
       }
+
       console.log('New user created:', data);
-      // Reset the user_name/email/password state after successful user creation
-      setUsername('');
-      setEmail('');
-      setPassword('');
-      setRepeatPassword('');
+      setUserData({
+        user_name: '',
+        email: '',
+        password: '',
+        repeatPassword: '',
+      });
     } catch (error) {
-      // Set the error state to display a generic error message
-      console.log(error);
-      setError('Error during user creation. Please try again.');
       console.error('Error during user creation:', error);
+      setError('Error during user creation. Please try again.');
     } finally {
       setLoading(false);
     }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUserData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
   };
 
   return (
@@ -89,35 +92,34 @@ const CreateUser = () => {
       <label>Username:</label>
       <input
         type="text"
-        value={user_name}
-        onChange={(e) => setUsername(e.target.value)}
+        name="user_name"
+        value={userData.user_name}
+        onChange={handleChange}
       />
       <label>Email:</label>
       <input
         type="text"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
+        name="email"
+        value={userData.email}
+        onChange={handleChange}
       />
       <label>Password:</label>
       <input
         type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
+        name="password"
+        value={userData.password}
+        onChange={handleChange}
       />
       <label>Repeat Password:</label>
       <input
         type="password"
-        value={repeatPassword}
-        onChange={(e) => setRepeatPassword(e.target.value)}
+        name="repeatPassword"
+        value={userData.repeatPassword}
+        onChange={handleChange}
       />
-      {error &&
-        error.map((err, index) => (
-          <div key={index} style={{ color: 'red' }}>
-            {err.msg}
-          </div>
-        ))}
+      {error && <div style={{ color: 'red' }}>{error}</div>}
       <button onClick={handleCreateUser} disabled={loading}>
-        {loading ? `Creating User: ${user_name}` : 'Create User'}
+        {loading ? `Creating User: ${userData.user_name}` : 'Create User'}
       </button>
     </div>
   );
