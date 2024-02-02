af164c3dac24f97502affc4878b628570d286a8b
SynthCyraX <SynthCyraX@gmail.com>
2024-02-02 03:58:14 +0100

    Added ListItemCard

---
+++ frontend/src/components/ListItemCard.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ListItemCard({ props }) {
+  return <div>{props.user_name}</div>;
+}
--- frontend/src/components/user/UserList.jsx
+++ frontend/src/components/user/UserList.jsx
@@ -1,11 +1,15 @@
 import { Link } from 'react-router-dom';
+import ListItemCard from '../ListItemCard';
 
 export default function UserList({ users }) {
   return (
     <ul>
       {users.map((user) => (
         <li key={user._id}>
-          <Link to={`/user/${user._id}`}>{user.user_name}</Link>
+          <Link to={`/user/${user._id}`}>
+            <ListItemCard user_name="user_name"></ListItemCard>
+            {user.user_name}
+          </Link>
         </li>
       ))}
     </ul>
