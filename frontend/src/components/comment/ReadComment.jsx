72f0000d10d721abdd34f8f675774a4c8863b6c8
SynthCyraX <SynthCyraX@gmail.com>
2024-02-03 14:06:03 +0100

    Added styles to ReadComment and ReadPost

---
+++ frontend/src/components/comment/ReadComment.jsx
@@ -0,0 +1,99 @@
+import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import styles from './ReadComment.module.css';
+
+import UnLikeComment from '../comment/UnLikeComment';
+import LikeComment from '../comment/LikeComment';
+import UpdateComment from '../comment/UpdateComment';
+import DeleteComment from '../comment/DeleteComment';
+
+export default function CommentSite() {
+  const [commentData, setCommentData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [isAuthor, setIsAuthor] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+  // id from params
+  const loaderData = useLoaderData();
+  const commentId = loaderData.commentid;
+
+  // id from logged in user
+  const authToken = localStorage.getItem('authToken');
+  // Split the payload of the jwt and convert the ._id part
+  const payload = JSON.parse(atob(authToken.split('.')[1]));
+  // Define the username you are looking for
+  const loggedInUserId = payload._id;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // Parameters for the backend request
+      const requestOptions = {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+          'Content-Type': 'application/json',
+        },
+      };
+
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `http://localhost:3000/api/comment/${commentId}`,
+          requestOptions
+        );
+        const data = await response.json();
+        setCommentData(data.searchedComment);
+
+        const isAuthorOfComment = searchForAuthor(
+          data.searchedComment.author_id,
+          loggedInUserId
+        );
+        setIsAuthor(isAuthorOfComment);
+
+        const isLikingComment = searchForLikes(
+          data.searchedComment.likes_id,
+          loggedInUserId
+        );
+        setIsLiking(isLikingComment);
+      } catch (error) {
+        console.error('Error while fetching comment:', error);
+      } finally {
+        setLoading(false);
+        // remove show...
+      }
+    };
+
+    fetchData();
+  }, [commentId, isLiking, authToken, loggedInUserId]);
+
+  //   const handleShowFollower = () => {
+  //     showFollower ? setShowFollower(false) : setShowFollower(true);
+  //   };
+
+  function searchForAuthor(author, loggedInUserId) {
+    return author._id === loggedInUserId;
+  }
+  function searchForLikes(arr, loggedInUserId) {
+    return arr.some((obj) => obj === loggedInUserId);
+  }
+
+  return (
+    <div className={styles.commentContainer}>
+      {loading && <div></div>}
+      {commentData && (
+        <>
+          <p>ID: {commentData._id}</p>
+          <p>Content: {commentData.content}</p>
+          <p>Author: {commentData.author_id.user_name}</p>
+          <p>Has {commentData.likes_id.length} likes</p>
+          {isLiking ? (
+            <UnLikeComment commentId={commentId} setIsLiking={setIsLiking} />
+          ) : (
+            <LikeComment commentId={commentId} setIsLiking={setIsLiking} />
+          )}
+          {isAuthor ? <UpdateComment commentId={commentId} /> : ''}
+          {isAuthor ? <DeleteComment commentId={commentId} /> : ''}
+        </>
+      )}
+    </div>
+  );
+}
---
+++ frontend/src/components/post/ReadPost.jsx
@@ -0,0 +1,114 @@
+import { useEffect, useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import styles from './ReadPost.module.css';
+import CommentList from '../comment/CommentList';
+import CreateComment from '../comment/CreateComment';
+import UnLikePost from '../post/UnLikePost';
+import LikePost from '../post/LikePost';
+import UpdatePost from '../post/UpdatePost';
+import DeletePost from '../post/DeletePost';
+
+export default function ReadPost() {
+  const [postData, setPostData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showComments, setShowComments] = useState(false);
+
+  const [isAuthor, setIsAuthor] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+
+  // id from params
+  const loaderData = useLoaderData();
+  const postId = loaderData.postid;
+
+  // id from logged in user
+  const authToken = localStorage.getItem('authToken');
+  // Split the payload of the jwt and convert the ._id part
+  const payload = JSON.parse(atob(authToken.split('.')[1]));
+  // Define the username you are looking for
+  const loggedInUserId = payload._id;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // Parameters for the backend request
+      const requestOptions = {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+          'Content-Type': 'application/json',
+        },
+      };
+
+      try {
+        setLoading(true);
+        const response = await fetch(
+          `http://localhost:3000/api/post/${postId}`,
+          requestOptions
+        );
+        const data = await response.json();
+        setPostData(data.searchedPost);
+
+        const isAuthorOfPost = searchForAuthor(
+          data.searchedPost.author_id,
+          loggedInUserId
+        );
+        setIsAuthor(isAuthorOfPost);
+
+        const isLikingPost = searchForLikes(
+          data.searchedPost.likes_id,
+          loggedInUserId
+        );
+        setIsLiking(isLikingPost);
+      } catch (error) {
+        console.error('Error while fetching user:', error);
+      } finally {
+        setLoading(false);
+        // remove show...
+      }
+    };
+
+    fetchData();
+  }, [postId, isLiking, authToken, loggedInUserId]);
+
+  const handleShowComments = () => {
+    showComments ? setShowComments(false) : setShowComments(true);
+  };
+  //   const handleShowFollower = () => {
+  //     showFollower ? setShowFollower(false) : setShowFollower(true);
+  //   };
+
+  function searchForAuthor(author, loggedInUserId) {
+    return author._id === loggedInUserId;
+  }
+  function searchForLikes(arr, loggedInUserId) {
+    return arr.some((obj) => obj === loggedInUserId);
+  }
+
+  return (
+    <div className={styles.postContainer}>
+      {loading && <div></div>}
+      {postData && (
+        <>
+          <p>ID: {postData._id}</p>
+          <p>Content: {postData.content}</p>
+          <p>Author: {postData.author_id.user_name}</p>
+          <p>Has {postData.comments_id.length} Comments:</p>
+          {showComments && postData.comments_id && (
+            <CommentList comments={postData.comments_id} />
+          )}
+          <button onClick={handleShowComments}>
+            {showComments ? 'Hide' : 'Show'}
+          </button>
+          <p>Has {postData.likes_id.length} likes</p>
+          {isLiking ? (
+            <UnLikePost postId={postId} setIsLiking={setIsLiking} />
+          ) : (
+            <LikePost postId={postId} setIsLiking={setIsLiking} />
+          )}
+          <CreateComment postId={postId} />
+          {isAuthor ? <UpdatePost postId={postId} /> : ''}
+          {isAuthor ? <DeletePost postId={postId} /> : ''}
+        </>
+      )}
+    </div>
+  );
+}
