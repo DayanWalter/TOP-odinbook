b31c7519f42c0f7b7fa0f50037ce260a8f577f9d
SynthCyraX <SynthCyraX@gmail.com>
2024-02-07 05:22:30 +0100

    Author, date and likes.length available for comments under posts

--- frontend/src/components/comment/CommentList.jsx
+++ frontend/src/components/comment/CommentList.jsx
@@ -6,14 +6,7 @@ export default function CommentList({ comments }) {
     <ul>
       {comments.map((comment) => (
         <li key={comment._id}>
-          <Link to={`/comment/${comment._id}`}>
-            <CommentListCard
-              author={comment.author_id.user_name}
-              posting_date={comment.posting_date}
-              likes={comment.likes_id}
-              content={comment.content}
-            />
-          </Link>
+          <CommentListCard commentId={comment._id} />
         </li>
       ))}
     </ul>
--- frontend/src/components/comment/CommentListCard.jsx
+++ frontend/src/components/comment/CommentListCard.jsx
@@ -3,32 +3,104 @@ import styles from '../../css/CommentListCard.module.css';
 import Icon from '@mdi/react';
 import { mdiCalendarMonthOutline } from '@mdi/js';
 import { mdiHeartOutline } from '@mdi/js';
+import { useEffect, useState } from 'react';
 
-export default function CommentListCard({
-  author,
-  content,
-  likes,
-  posting_date,
-}) {
+export default function CommentListCard({ commentId }) {
+  const [commentData, setCommentData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const [isAuthor, setIsAuthor] = useState(false);
+
+  const [isOpenModal, setIsOpenModal] = useState(false);
+  const [isOpenCommentCreate, setIsOpenCommentCreate] = useState(false);
+  const [isOpenCommentList, setIsOpenCommentList] = useState(false);
+  const [isLiking, setIsLiking] = useState(false);
+
+  // id from logged in user
+  const authToken = localStorage.getItem('authToken');
+  // Split the payload of the jwt and convert the ._id part
+  const payload = JSON.parse(atob(authToken.split('.')[1]));
+  // Define the username you are looking for
+  const loggedInUserId = payload._id;
+
+  function searchForAuthor(author, loggedInUserId) {
+    return author._id === loggedInUserId;
+  }
+
+  function searchForLikes(arr, loggedInUserId) {
+    return arr.some((obj) => obj === loggedInUserId);
+  }
+
+  const fetchCommentData = async () => {
+    // Parameters for the backend request
+    const requestOptions = {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        'Content-Type': 'application/json',
+      },
+    };
+
+    try {
+      setLoading(true);
+      const response = await fetch(
+        `http://localhost:3000/api/comment/${commentId}`,
+        requestOptions
+      );
+      const data = await response.json();
+      setCommentData(data.searchedComment);
+
+      const isAuthorOfComment = searchForAuthor(
+        data.searchedComment.author_id,
+        loggedInUserId
+      );
+      setIsAuthor(isAuthorOfComment);
+
+      const isLikingComment = searchForLikes(
+        data.searchedComment.likes_id,
+        loggedInUserId
+      );
+      setIsLiking(isLikingComment);
+    } catch (error) {
+      console.error('Error while fetching comment:', error);
+    } finally {
+      setLoading(false);
+      // remove show...
+    }
+  };
+
+  useEffect(() => {
+    fetchCommentData();
+  }, [commentId, isLiking, authToken, loggedInUserId]);
+
   return (
-    <div className={styles.card}>
-      {/* <div className={styles.profilePicture}> */}
-      {/* </div> */}
-      <div className={styles.stats}>
-        <div className={styles.author}>{author}</div>
-        <div className={styles.content}>{content}</div>
-        <div className={styles.footer}>
-          <div className={styles.iconGroup}>
-            <Icon path={mdiHeartOutline} size={1} />
-            <div>{likes.length}</div>
-          </div>
+    <>
+      {loading && <div></div>}
+      {commentData && (
+        <>
+          <div className={styles.card}>
+            {/* <div className={styles.profilePicture}> */}
+            {/* </div> */}
+            <div className={styles.stats}>
+              <div className={styles.author}>
+                {commentData.author_id.user_name}
+              </div>
+              <div className={styles.content}>{commentData.content}</div>
+              <div className={styles.footer}>
+                <div className={styles.iconGroup}>
+                  <Icon path={mdiHeartOutline} size={1} />
+                  <div>{commentData.likes_id.length}</div>
+                </div>
 
-          <div className={styles.iconGroup}>
-            <Icon path={mdiCalendarMonthOutline} size={1} />
-            {new Date(posting_date).toLocaleDateString()}
+                <div className={styles.iconGroup}>
+                  <Icon path={mdiCalendarMonthOutline} size={1} />
+                  {new Date(commentData.posting_date).toLocaleDateString()}
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </div>
+        </>
+      )}
+    </>
   );
 }
