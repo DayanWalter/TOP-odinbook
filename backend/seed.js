cbc9829ad8563580b97374338aa135e929eced5b
SynthCyraX <SynthCyraX@gmail.com>
2024-02-09 04:57:48 +0100

    Added seed.js for user and post gen

---
+++ backend/seed.js
@@ -0,0 +1,73 @@
+// require the necessary libraries
+const faker = require('@faker-js/faker');
+const mongoose = require('mongoose');
+const User = require('./models/user');
+const Post = require('./models/post');
+
+async function seedData() {
+  // Connection URL
+  const uri =
+    'mongodb+srv://synthcyrax:UAlQLzUjRekhEcaf@cluster0.q8cadaa.mongodb.net/?retryWrites=true&w=majority';
+  const seed_count = 2;
+  mongoose.set('strictQuery', false);
+  mongoose
+    .connect(uri, {
+      // useNewUrlParser: true,
+      // useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('Connected to db');
+    })
+    .catch((err) => {
+      console.log('error', err);
+    });
+
+  let userData = [];
+  let postsData = [];
+  // create 5000 fake data
+  for (let i = 0; i < seed_count; i++) {
+    const user_name = faker.faker.person.firstName();
+    const email = faker.faker.internet.email();
+    const password = faker.faker.internet.password();
+    const location = faker.faker.location.city();
+    const bio = faker.faker.person.bio();
+    const img_url = faker.faker.image.url();
+    const avatar_url = faker.faker.image.urlLoremFlickr();
+    const reg_date = faker.faker.date.past();
+    const user = new User({
+      user_name,
+      email,
+      password,
+      location,
+      bio,
+      img_url,
+      avatar_url,
+      reg_date,
+    });
+
+    userData.push(user);
+    for (let j = 0; j < seed_count; j++) {
+      const content = faker.faker.hacker.phrase();
+      const posting_date = faker.faker.date.past();
+      const author_id = user._id;
+
+      const post = new Post({ author_id, content, posting_date });
+      postsData.push(post);
+
+      // Push post _id to user's posts_id array
+      user.posts_id.push(post._id);
+    }
+  }
+
+  const seedDB = async () => {
+    await User.insertMany(userData);
+    await Post.insertMany(postsData);
+  };
+
+  seedDB().then(() => {
+    mongoose.connection.close();
+    console.log('seed success');
+  });
+}
+
+seedData();
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,20 +4,8 @@ import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 // Sites
 import ProfileSite from './components/sites/ProfileSite.jsx';
-import PostSite from './components/sites/PostSite.jsx';
-import CommentSite from './components/sites/CommentSite.jsx';
 import SignUpSite from './components/sites/SignUpSite.jsx';
 import LoginSite from './components/sites/LoginSite.jsx';
-import ErrorSite from './components/sites/ErrorSite.jsx';
-// User
-import UserCreate from './components/user/UserCreate.jsx';
-import UserLogin from './components/user/UserLogin.jsx';
-import UserDelete from './components/user/UserDelete.jsx';
-import UsersRead from './components/user/UsersRead.jsx';
-import UserUpdate from './components/user/UserUpdate.jsx';
-// Post
-import PostCreate from './components/post/PostCreate.jsx';
-import PostFeed from './components/post/PostFeed.jsx';
 
 const router = createBrowserRouter([
   // User
