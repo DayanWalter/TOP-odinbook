d4f3e30c999440f410e141ebfaa712a4f60ceece
Dayan Walter <dayanwalter@gmail.com>
2024-02-26 04:06:33 +0100

    Changed gap PostList and CommentList

--- frontend/src/components/comment/CommentList.jsx
+++ frontend/src/components/comment/CommentList.jsx
@@ -4,7 +4,7 @@ export default function CommentList({ comments }) {
   comments.sort((a, b) => new Date(b.posting_date) - new Date(a.posting_date));
 
   return (
-    <ul>
+    <ul className="grid gap-3 mt-3 ml-5">
       {comments.map((comment) => (
         <li key={comment._id}>
           <CommentListCard commentId={comment._id} />
--- frontend/src/components/post/PostList.jsx
+++ frontend/src/components/post/PostList.jsx
@@ -3,7 +3,7 @@ import PostListCard from './PostListCard';
 export default function PostList({ posts }) {
   posts.sort((a, b) => new Date(b.posting_date) - new Date(a.posting_date));
   return (
-    <ul className="grid gap-3">
+    <ul className="grid gap-5">
       {posts.map((post) => (
         <li key={post._id}>
           <PostListCard postId={post._id} />
