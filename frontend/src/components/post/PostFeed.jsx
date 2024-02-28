f0af7b2d15a96dca3cf2d3b4c341d6778f82f762
Dayan Walter <dayanwalter@gmail.com>
2024-02-28 08:40:30 +0100

    Removed PostList.jsx

--- frontend/src/components/post/PostFeed.jsx
+++ frontend/src/components/post/PostFeed.jsx
@@ -1,5 +1,5 @@
-import PostList from './PostList';
 import useFetchFeed from '../../hooks/useFetchFeed';
+import PostListCard from './PostListCard';
 
 export default function PostFeed() {
   const { data: feed, loading, error } = useFetchFeed();
@@ -8,7 +8,19 @@ export default function PostFeed() {
     <>
       {error && <div style={{ color: 'red' }}>{error}</div>}
       {loading && <p>Loading feed...</p>}
-      {feed && <PostList posts={feed} />}
+      {feed && (
+        <ul className="grid gap-5">
+          {feed.length === 0 && 'Start writing :)'}
+
+          {feed
+            .sort((a, b) => new Date(b.posting_date) - new Date(a.posting_date))
+            .map((post) => (
+              <li key={post._id}>
+                <PostListCard postId={post._id} />
+              </li>
+            ))}
+        </ul>
+      )}
     </>
   );
 }
--- frontend/src/components/sites/Home.jsx
+++ frontend/src/components/sites/Home.jsx
@@ -2,11 +2,6 @@ import PostFeed from '../post/PostFeed';
 import MainSite from './MainSite';
 
 export default function Home() {
-  const token = localStorage.getItem('authToken');
-  const payload = JSON.parse(atob(token.split('.')[1]));
-  // Define the username you are looking for
-  const userId = payload._id;
-  const userName = payload.user_name;
   return (
     <MainSite>
       <PostFeed />
