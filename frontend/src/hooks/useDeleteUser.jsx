5126c49ab484d2ab7b8e1b3b3b5547994356a7ef
Dayan Walter <dayanwalter@gmail.com>
2024-03-01 04:58:48 +0100

    useDeleteUser.jsx first draft

---
+++ frontend/src/hooks/useDeleteUser.jsx
@@ -0,0 +1,39 @@
+import { useState } from 'react';
+
+export default function useDeleteUser() {
+  const BASE_URL = import.meta.env.VITE_SERVER_URL;
+
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const authToken = localStorage.getItem('authToken');
+
+  const deleteUser = async () => {
+    try {
+      setLoading(true);
+
+      // Log an error if authentication token is not available
+      if (!authToken) {
+        console.error('Authentication token not available.');
+        setError('Authentication token not available.');
+        return;
+      }
+
+      const response = await fetch(`${BASE_URL}/api/user/delete`, {
+        method: `DELETE`,
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const responseJSON = await response.json();
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { deleteUser, loading, error };
+}
