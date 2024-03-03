09d89896368f6a39fc6c61016433ea97a705cb79
Dayan Walter <dayanwalter@gmail.com>
2024-03-03 06:30:08 +0100

    Added ContrubiteCard

---
+++ frontend/src/components/sites/ContributeCard.jsx
@@ -0,0 +1,26 @@
+import { Link } from 'react-router-dom';
+import useFetchLoggedInUser from '../../hooks/useFetchLoggedInUser';
+
+export default function ContributeCard() {
+  return (
+    <>
+      <div className="fixed hidden w-1/3 h-64 bg-white border rounded xl:w-1/4 sm:block">
+        <div className="flex flex-col items-center justify-center w-full gap-5 mt-6">
+          <img src="./bitfeather.png" alt="bitfeather logo" />
+          <h2 className="text-3xl font-semibold text-primary">BitFeather</h2>
+          <p className="text-xs">The place where you can chirp out loud...</p>
+
+          <Link
+            target="_blank"
+            rel="noopener noreferrer"
+            to={`https://www.github.com/`}
+          >
+            <p className="underline underline-offset-2 text-primary hover:text-primary/80">
+              View Code
+            </p>
+          </Link>
+        </div>
+      </div>
+    </>
+  );
+}
--- frontend/src/components/sites/Home.jsx
+++ frontend/src/components/sites/Home.jsx
@@ -1,4 +1,5 @@
 import PostFeed from '../post/PostFeed';
+import ContributeCard from './ContributeCard';
 import MainSite from './MainSite';
 import ProfileCard from './ProfileCard';
 
@@ -6,12 +7,14 @@ export default function Home() {
   return (
     <MainSite>
       <div className="grid px-16 gap-x-8 sm:grid-cols-2 xl:grid-cols-3">
-        <div className="w-full">
+        <div className="w-full ">
           <ProfileCard />
         </div>
 
         <PostFeed />
-        <div className="border">Empty col</div>
+        <div className="flex justify-end w-full ">
+          <ContributeCard />
+        </div>
       </div>
     </MainSite>
   );
